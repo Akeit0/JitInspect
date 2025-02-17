86e494a65d91b4050b756bd335532a32ec5a327f
Akeit0 <90429982+Akeit0@users.noreply.github.com>
2025-02-17 11:45:24 +0900

    Add: comments

--- src/JitInspect/DisassembleExtensions.cs
+++ src/JitInspect/DisassembleExtensions.cs
@@ -2,8 +2,14 @@
 
 namespace JitInspect;
 
+/// <summary>
+/// Provides extension methods for disassembling methods.
+/// </summary>
 public static class DisassembleExtensions
 {
+    /// <summary>
+    /// Disassembles the given method.
+    /// </summary>
     public static string Disassemble(this MethodBase methodBase)
     {
         using var disassembler = JitDisassembler.Create();
