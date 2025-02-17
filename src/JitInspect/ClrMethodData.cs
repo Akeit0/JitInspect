d5780f40ebcc6939f318f49f920ce6b7cc005fd4
Akeit0 <90429982+Akeit0@users.noreply.github.com>
2025-02-17 15:44:24 +0900

    Update : to 1.0.1

--- src/JitInspect/ClrMethodData.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace JitInspect;
-
-internal readonly struct ClrMethodData(ulong methodAddress, uint methodSize)
-{
-    public ulong MethodAddress { get; } = methodAddress;
-    public uint MethodSize { get; } = methodSize;
-}
