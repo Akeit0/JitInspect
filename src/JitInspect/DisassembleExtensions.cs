438065e67c700fa64871010ca9997d9885edb7f3
Akeit0 <90429982+Akeit0@users.noreply.github.com>
2025-02-20 10:22:25 +0900

    Add: Disassemble overloads

--- src/JitInspect/DisassembleExtensions.cs
+++ src/JitInspect/DisassembleExtensions.cs
@@ -10,9 +10,24 @@ public static class DisassembleExtensions
     /// <summary>
     /// Disassembles the given method.
     /// </summary>
-    public static string Disassemble(this MethodBase methodBase)
+    public static string Disassemble(this MethodBase methodBase, DisassembleOptions? options =null)
     {
         using var disassembler = JitDisassembler.Create();
-        return disassembler.Disassemble(methodBase);
+        return disassembler.Disassemble(methodBase, options);
+    }
+    
+    /// <summary>
+    /// Disassembles the given method.
+    /// </summary>
+    public static string Disassemble(this MethodBase methodBase, bool writeMethodSignature, bool writeILToNativeMap = false, DisassemblySyntax syntax = DisassemblySyntax.Masm, int maxRecursiveDepth = 0)
+    {
+        using var disassembler = JitDisassembler.Create();
+        return disassembler.Disassemble(methodBase, new DisassembleOptions
+        {
+            WriteMethodSignature = writeMethodSignature,
+            WriteILToNativeMap = writeILToNativeMap,
+            Syntax = syntax,
+            MaxRecursiveDepth = maxRecursiveDepth,
+        });
     }
 }
